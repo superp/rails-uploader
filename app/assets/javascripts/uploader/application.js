e1b619459f326abaa636e6aaed860a4410bb89e4
John Bintz <john@coswellproductions.com>
2013-01-11 07:33:11 -0500

    move to jquery-ui-rails

--- app/assets/javascripts/uploader/application.js
+++ app/assets/javascripts/uploader/application.js
@@ -1,7 +1,7 @@
+//= require jquery.ui.widget
 //= require uploader/locales/en
 //= require uploader/tmpl.min
 //= require uploader/load-image.min
-//= require uploader/jquery.ui.widget
 //= require uploader/jquery.iframe-transport
 //= require uploader/jquery.fileupload
 //= require uploader/jquery.fileupload-ui
